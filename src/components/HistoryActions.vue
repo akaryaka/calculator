e55fcca7b262f644c5fdf234446574bbd64be243
akariaka@yandex.ru <akariaka@yandex.ru>
2026-05-20 21:35:36 +0600

    html code

--- src/components/HistoryActions.vue
+++ src/components/HistoryActions.vue
@@ -1,5 +1,18 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { history } from '../constants';
+</script>
 
-<template></template>
+<template>
+  <section class="history">
+    <h2>История операций</h2>
+    <div class="output">
+      <ul>
+        <li v-for="item in history">{{ item }}</li>
+      </ul>
+    </div>
+  </section>
+</template>
 
-<style></style>
+<style>
+
+</style>
